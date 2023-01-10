da1cfccd3e5813dfcd528845d5954cf60ea329d4
Brianna DelValle <Briannadelvalle@gmail.com>
2023-01-10 16:58:23 -0500

    Update phone numbers

--- app/Http/Controllers/IvrController.php
+++ app/Http/Controllers/IvrController.php
@@ -85,9 +85,9 @@ class IvrController extends Controller
         );
 
         $planetNumbers = [
-            '2' => '+12024173378',
-            '3' => '+12027336386',
-            '4' => '+12027336637'
+            '2' => '+19295566487',
+            '3' => '+17262043675',
+            '4' => '+16513582243'
         ];
         $selectedOption = $request->input('Digits');
 
