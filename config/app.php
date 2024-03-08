4754a2635f41361114918c3dc1b209184854f06f
amucarsel <amucarsel@twilio.com>
2024-03-08 15:51:54 -0500

    Laravel bumped to 5.7.x. Tests passing

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
 
         /*
          * Application Service Providers...
